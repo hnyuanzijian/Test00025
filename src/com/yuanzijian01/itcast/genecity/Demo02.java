221ea8bdf51862fec705c131eced192bf97af9c9
hnyuanzijian <18738395879@163.com>
2017-06-04 17:00:45 +0800

    泛型，输入输出，线程

---
+++ src/com/yuanzijian01/itcast/genecity/Demo02.java
@@ -0,0 +1,34 @@
+package com.yuanzijian01.itcast.genecity;
+
+/*
+ * 自定义泛型： 自定义泛型可以理解为是一个数据类型的占位符，或者是理解为是一个数据类型的变量。
+ 
+ 泛型方法：
+	
+	泛型方法的定义格式： 
+		
+		修饰符   <声明自定义泛型>返回值类型       函数名(形参列表..){
+		
+		}
+
+注意：
+	1. 在方法上的自定义泛型的具体数据类型是调用该方法的时候传入实参的时候确定的。
+	2. 自定义泛型使用的标识符只要符合标识符的命名规则即可。
+
+
+
+ 需求： 定义一个函数可以接收任意类型的参数，要求函数的返回值类型与实参的数据类型要一致。
+ * */
+
+public class Demo02 {
+	public static void main(String[] args) {
+		 Integer i = print(12);
+		 String str = print("abc");
+		 
+		 System.out.println(str);
+	}
+	
+	public static <abc> abc print(abc o){
+		return o;
+	}
+}
---
+++ src/com/yuanzijian01/itcast/genecity/Demo03.java
@@ -0,0 +1,34 @@
+package com.yuanzijian01.itcast.genecity;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/*
+ 泛型类
+ 
+ 泛型类的定义格式：
+ 	
+ 	class 类名<声明自定义的泛型>{
+ 	
+ 	} 
+ 
+注意的事项： 
+ 	1. 在类上自定义的泛型的具体数据类型是在创建对象的时候指定的。
+	2. 在类上自定义了泛型，如果创建该类的对象时没有指定泛型的具体类型，那么默认是Object类型。
+ 	
+ 	
+ 	
+ */
+
+class Worker implements Comparator<Worker>{
+
+	@Override
+	public int compare(Worker o1, Worker o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+	
+}
